01f6df53685eb706cd3bb06076b49ecdb24bf498
Emmanuel Mensah <immaxp183@gmail.com>
2022-11-21 20:06:02 +0100

    Laying out the interface

---
+++ Fresh Reminder/Model/Reminder.swift
@@ -0,0 +1,21 @@
+//
+//  Reminder.swift
+//  Fresh Reminder
+//
+//  Created by macbook on 21/11/2022.
+//
+
+import Foundation
+
+
+class Reminder {
+    var title: String
+    var date: Date
+    var isCompleted:Bool
+    
+    init(title: String, date: Date, isCompleted: Bool) {
+        self.title = title
+        self.date = date
+        self.isCompleted = isCompleted
+    }
+}
